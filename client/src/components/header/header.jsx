c3d8750105aa66e5045b6be27ca45bf404627f0f
Sally Sun <sallyysun101@gmail.com>
2022-10-25 22:44:53 +1100

    Changed css so it works now (#2)
    * distance between drivers and people
    
    * search not working
    
    * kinda wokring
    
    * change long center
    
    * updated css
    
    * fixed merge conflict
    
    * removed unnecesary files
    
    * search css
    
    * added pin and radius
    
    * added drawer
    
    * changed style
    
    * styles placeholder combobox
    
    Co-authored-by: Mun Joon Teo <teomunjoon101@gmail.com>

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,22 +1,15 @@
 
 import Title from './title'
-// import Search from './search'
-import UploadCSV from './uploadCSV'
 
 import 'antd/dist/antd.css';
 import './header.css'
 
-function Header() {
+export default function Header() {
   	return (
 		<div className='container'>
 			<div className="header">
 				<Title/>
-				<div className="header__space">
-					<UploadCSV />
-				</div>
 			</div>
 		</div>
   	)
 }
-
-export default Header
---
+++ client/src/components/map/search.jsx
@@ -0,0 +1,50 @@
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
+
+export default function Search({ setAddress }) {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
+
+  const handleSelect = async (val) => {
+    setValue(val, false);
+    clearSuggestions();
+    const results = await getGeocode({ address: val });
+    const { lat, lng } = await getLatLng(results[0]);
+    setAddress({ lat, lng });
+  };
+
+  return (
+    <Combobox onSelect={handleSelect}>
+      <ComboboxInput
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        disabled={!ready}
+        className="map__combobox"
+        placeholder="Search Address"
+      />
+      <ComboboxPopover>
+        <ComboboxList>
+          {status === "OK" &&
+            data.map(({ place_id, description }) => (
+              <ComboboxOption key={place_id} value={description} />
+            ))}
+        </ComboboxList>
+      </ComboboxPopover>
+    </Combobox>
+  );
+}
