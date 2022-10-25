7ac2fbe28aa85c9d760a86ebd122ecbe52fea776
Sally Sun <sallyysun101@gmail.com>
2022-10-25 20:51:59 +1100

    fixed merge conflict

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,10 +1,5 @@
 
 import Title from './title'
-<<<<<<< HEAD
-=======
-// import Search from './search'
-import UploadCSV from './uploadCSV'
->>>>>>> 8cf0795100f6c59246c565d8eacdfe1f26011597
 
 import 'antd/dist/antd.css';
 import './header.css'
@@ -14,12 +9,6 @@ function Header() {
 		<div className='container'>
 			<div className="header">
 				<Title/>
-<<<<<<< HEAD
-=======
-				<div className="header__space">
-					<UploadCSV />
-				</div>
->>>>>>> 8cf0795100f6c59246c565d8eacdfe1f26011597
 			</div>
 		</div>
   	)
--- client/src/components/map/map.jsx
+++ client/src/components/map/map.jsx
@@ -3,21 +3,12 @@ import {
   GoogleMap
 } from "@react-google-maps/api"
 
-<<<<<<< HEAD
 import Search from "./search";
 import UploadCSV from "./uploadCSV";
 import './map.css'
 
 function Map() {
 	const center = useMemo(() => ({ lat: -33.85, lng: 151 }), []);
-=======
-import Search from "../header/search";
-import '../../index.css'
-import './map.css'
-
-function Map() {
-	const center = useMemo(() => ({ lat: -33.85, lng: 151.21 }), []);
->>>>>>> 8cf0795100f6c59246c565d8eacdfe1f26011597
 	const [address, setAddress] = useState();
 	const mapRef = useRef();
 	const onLoad = useCallback((map) => (mapRef.current = map), []);
@@ -25,7 +16,6 @@ function Map() {
 	return (
 		<div className="container">
 			<div className="map">
-<<<<<<< HEAD
                 <div className="map__info">
                     <Search setAddress={(position) => {
                         setAddress(position);
@@ -37,24 +27,10 @@ function Map() {
 					zoom={11}
 					center={center}
 					mapContainerClassName='map__container'
-=======
-				<Search setAddress={(position) => {
-					setAddress(position);
-					mapRef.current?.panTo(position);
-				}} />
-				<GoogleMap 
-					zoom={11}
-					center={center}
-					mapContainerClassName='map-container'
->>>>>>> 8cf0795100f6c59246c565d8eacdfe1f26011597
 					onLoad={onLoad}
 				>
 				</GoogleMap>
 			</div>
-<<<<<<< HEAD
-=======
-
->>>>>>> 8cf0795100f6c59246c565d8eacdfe1f26011597
 		</div>
 	)
 }
