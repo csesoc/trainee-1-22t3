44d3f1e294eece01c576f38a3f592688ff067079
Sally Sun <sallyysun101@gmail.com>
2022-11-10 14:52:28 +1100

    Plotted points based on drivers and passengers (#5)
    it's my birthday tomorrow

--- client/src/components/map/map.jsx
+++ client/src/components/map/map.jsx
@@ -1,30 +1,74 @@
 import { useState, useMemo, useRef, useCallback } from "react";
-import { GoogleMap, Marker, Circle } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  Marker,
+  Circle,
+  MarkerClusterer,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
 
-// import Search from "./search";
 import UploadCSV from "./uploadCSV";
 import "./map.css";
 
-export default function Map() {
+const Map = () => {
   const center = useMemo(() => ({ lat: -33.85, lng: 151 }), []);
-  const [address, setAddress] = useState();
+  //   const [drivers, setDrivers] = useState();
+  //   const [passengers, setPassengers] = useState();
+  const [directions, setDirections] = useState();
+  const [style, setStyle] = useState(false);
   const mapRef = useRef();
   const onLoad = useCallback((map) => (mapRef.current = map), []);
   const options = useMemo(
     () => ({
-      // mapId: "b181cac70f27f5e6", // dark map
+      //   mapId: "b181cac70f27f5e6", // dark map
       mapId: "2ca571bbe60acfd2", // light map
-      //   disableDefaultUI: true,
+      disableDefaultUI: true,
       clickableIcons: false,
     }),
     []
   );
 
+  //   const fetchDirections = async (position) => {
+  //     if (!driver) return;
+  //     const service = new window.google.maps.DirectionsService();
+  //     service.route(
+  //       {
+  //         origin: driver,
+  //         destination: position,
+  //         travelMode: window.google.maps.TravelMode.DRIVING,
+  //       },
+  //       (result, status) => {
+  //         if (status === "OK" && result) {
+  //           setDirections(result);
+  //         }
+  //       }
+  //     );
+  //   };
+
+  const iconStyle = style
+    ? "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"
+    : "http://maps.google.com/mapfiles/ms/icons/blue-dot.png";
+
+  const passengers = [
+    { Name: "MJ", Suburb: { lat: -33.8234, lng: 151.1939 } },
+    { Name: "Raiyan", Suburb: { lat: -33.7961, lng: 151.178 } },
+    { Name: "Rachel", Suburb: { lat: -33.8368, lng: 151.2073 } },
+    { Name: "Oscar", Suburb: { lat: -33.7457, lng: 151.1432 } },
+    { Name: "James", Suburb: { lat: -33.7201, lng: 151.117 } },
+  ];
+
+  const drivers = [
+    { Name: "Sally", Suburb: { lat: -33.82, lng: 151.19 } },
+    { Name: "Hellen", Suburb: { lat: -33.9646, lng: 151.101 } },
+  ];
+
   return (
     <div className="container">
       <div className="map">
         <div className="map__info">
-          <UploadCSV />
+          <UploadCSV
+          //   setDrivers={setDrivers} setPassengers={setPassengers}
+          />
         </div>
         <GoogleMap
           zoom={11}
@@ -33,19 +77,74 @@ export default function Map() {
           onLoad={onLoad}
           options={options}
         >
-          {address && (
-            <>
-              <Marker position={address} />
-              <Circle center={address} radius={5000} options={closeOptions} />
-              <Circle center={address} radius={10000} options={middleOptions} />
-              <Circle center={address} radius={15000} options={farOptions} />
-            </>
-          )}
+          {/* {directions && (
+            <DirectionsRenderer
+              directions={directions}
+              options={{
+                polyLineOptions: {
+                  zIndex: 50,
+                  strokeColor: "#1976d2",
+                  strokeWeight: 5,
+                },
+              }}
+            />
+          )} */}
+          <MarkerClusterer>
+            {(clusterer) =>
+              passengers.map((person, i) => (
+                <Marker
+                  key={i}
+                  position={person.Suburb}
+                  title={`${i + 1}. ${person.Name}`}
+                  icon={iconStyle}
+                  clusterer={clusterer}
+                  onClick={() => {
+                    setStyle((current) => !current);
+                    // fetchDirections(position);
+                  }}
+                />
+              ))
+            }
+          </MarkerClusterer>
+          <MarkerClusterer>
+            {(clusterer) =>
+              drivers.map((person, i) => (
+                <>
+                  <Marker
+                    key={i}
+                    position={person.Suburb}
+                    title={`${i + 1}. ${person.Name}`}
+                    icon={
+                      "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
+                    }
+                    clusterer={clusterer}
+                  />
+                  <Circle
+                    center={person.Suburb}
+                    radius={5000}
+                    options={closeOptions}
+                  />
+                  <Circle
+                    center={person.Suburb}
+                    radius={10000}
+                    options={middleOptions}
+                  />
+                  <Circle
+                    center={person.Suburb}
+                    radius={15000}
+                    options={farOptions}
+                  />
+                </>
+              ))
+            }
+          </MarkerClusterer>
         </GoogleMap>
       </div>
     </div>
   );
-}
+};
+
+export default Map;
 
 // Map circle options
 const defaultOptions = {
